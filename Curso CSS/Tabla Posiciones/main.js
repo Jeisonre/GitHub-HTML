cb9083381027ba404bdc1918ea1fa52fc7064ad0
Jeisonre <jeisonespin25@gmail.com>
2022-11-24 17:00:18 -0500

    config

--- Curso CSS/Tabla Posiciones/main.js
+++ Curso CSS/Tabla Posiciones/main.js
@@ -1,31 +1,31 @@
-function nComentario(){
-    let li = document.createElement("li");
-    let valoringresado = document.getElementById("nuevoComentario").value;
-    let text = document.createTextNode(valoringresado);
-    li.appendChild(text);
+function nComentario() {
+  let li = document.createElement("li");
+  let valoringresado = document.getElementById("nuevoComentario").value;
+  let text = document.createTextNode(valoringresado);
+  li.appendChild(text);
 
-    if(valoringresado === ''){
-        alert("Ingrese un Comentario!");
-    }else{
-        document.getElementById("comentarios").appendChild(li);
-    }
+  if (valoringresado === "") {
+    alert("Ingrese un Comentario!");
+  } else {
+    document.getElementById("comentarios").appendChild(li);
+  }
 
-    document.getElementById("nuevoComentario").value = "";
-    li.cloneNode = "comentario";
+  document.getElementById("nuevoComentario").value = "";
+  li.cloneNode = "comentario";
 
-    let borrar = document.createElement("p");
-    borrar.innerHTML = ("Borrar");
-    borrar.className = "close";
-    li.appendChild(borrar);
+  let borrar = document.createElement("p");
+  borrar.innerHTML = "Borrar";
+  borrar.className = "close";
+  li.appendChild(borrar);
 
-    let close = document.getElementsByClassName("close");
-    let i;
-    for (i = 0; i<close.length; i++){
-        close[i].onclick = function(){
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
+  let close = document.getElementsByClassName("close");
+  let i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
 }
 
 // function usrpas(){
